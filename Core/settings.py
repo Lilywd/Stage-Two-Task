e71a392ea0b2a47e230643055e6bcee9bb49d7ef
lilywd <ndungulilianwanjiku@gmail.com>
2023-09-14 00:17:25 +0300

    deploy

--- Core/settings.py
+++ Core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!455d+u2=4_0*pm2migyx6f81h!b%xzhaa14@n4j#dsyoownzw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
